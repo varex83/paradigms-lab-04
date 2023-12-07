ea25c43807f04c9f38bdb32b98ec1cce694964cd
Bohdan Ohorodnii <limposfeed@gmail.com>
2023-12-07 14:03:27 +0200

    update to scalar_mul

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#pragma GCC optimize("O0")
+//#pragma GCC optimize("O0")
 #include <iostream>
 #include <chrono>
 #include <string>
@@ -30,75 +30,82 @@ public:
     }
 };
 
+
 vector<int> generateRandomVector(int n) {
     vector<int> v(n);
     mt19937 rng(random_device{}());
     uniform_int_distribution<int> dist(0, 1000000000);
     for (int i = 0; i < n; ++i) {
-        v[i] = dist(rng);
+        v[i] = dist(rng) % 1000000;
     }
     return v;
 }
 
-void replace_if_without_policy(vector<int>& v, int threshold, int newValue) {
-    Timer t("replace_if_without_policy");
-    replace_if(v.begin(), v.end(), [threshold](int x) { return x > threshold; }, newValue);
+void scalar_mul_without_policy(vector<int>& v, vector<int>& u) {
+    Timer t("scalar_mul_without_policy");
+    transform_reduce(v.begin(), v.end(), u.begin(), 0, plus<>(), multiplies<>());
 }
 
-//void replace_if_with_policy_par(vector<int>& v, int threshold, int newValue) {
-//    Timer t("replace_if_with_policy_par");
-//    replace_if(execution::par, v.begin(), v.end(), [threshold](int x) { return x > threshold; }, newValue);
+
+//void scalar_mul_with_policy_par(vector<int>& v, int threshold, int newValue) {
+//    Timer t("scalar_mul_with_policy_par");
+//    scalar_mul(execution::par, v.begin(), v.end(), [threshold](int x) { return x > threshold; }, newValue);
 //}
 //
-//void replace_if_with_policy_seq(vector<int>& v, int threshold, int newValue) {
-//    Timer t("replace_if_with_policy_seq");
-//    replace_if(execution::seq, v.begin(), v.end(), [threshold](int x) { return x > threshold; }, newValue);
+//void scalar_mul_with_policy_seq(vector<int>& v, int threshold, int newValue) {
+//    Timer t("scalar_mul_with_policy_seq");
+//    scalar_mul(execution::seq, v.begin(), v.end(), [threshold](int x) { return x > threshold; }, newValue);
 //}
 
-void replace_if_custom(vector<int>& v, int threshold, int newValue) {
-    Timer t("replace_if_custom");
-    for (int& x : v) {
-        if (x > threshold) {
-            x = newValue;
-        }
+void scalar_mul_custom(vector<int>& v, vector<int>& u) {
+    Timer t("scalar_mul_custom");
+    int result = 0;
+    for (int i = 0; i < v.size(); ++i) {
+        result += v[i] * u[i];
     }
 }
 
-void replace_if_custom_multithreaded(vector<int>& v, int threshold, int newValue, int threads) {
-    Timer t("replace_if_custom_multithreaded threads: " + to_string(threads));
+void scalar_mul_custom_multithreaded(vector<int>& v, vector<int>& u, int threads) {
+    Timer t("scalar_mul_custom_multithreaded threads: " + to_string(threads));
     vector<thread> workers;
     int chunkSize = v.size() / threads;
+    vector<int> results(threads);
     for (int i = 0; i < threads; ++i) {
-        workers.emplace_back([i, chunkSize, &v, threshold, newValue]() {
+        workers.emplace_back([i, chunkSize, &v, u, &results]() {
             int start = i * chunkSize;
-            int end = (i + 1) * chunkSize;
+            int end = std::min((int)v.size(), (i + 1) * chunkSize);
+            int result = 0;
             for (int j = start; j < end; ++j) {
-                if (v[j] > threshold) {
-                    v[j] = newValue;
-                }
+                result += v[j] * u[j];
             }
+            results[i] = result;
         });
     }
-    for (thread& t : workers) {
-        t.join();
+    int result = 0;
+    for (auto& worker : workers) {
+        worker.join();
     }
+
+    for (int i = 0; i < threads; ++i) {
+        result += results[i];
+    }
+
 }
 
 
 int main() {
     int n = 10000000;
-    int threshold = 500000;
-    int newValue = 0;
 
     vector<int> v = generateRandomVector(n);
-    replace_if_without_policy(v, threshold, newValue);
-//    replace_if_with_policy_par(v2, threshold, newValue);
-//    replace_if_with_policy_seq(v3, threshold, newValue);
-    replace_if_custom(v, threshold, newValue);
-    replace_if_custom_multithreaded(v, threshold, newValue, 1);
-    replace_if_custom_multithreaded(v, threshold, newValue, 2);
-    replace_if_custom_multithreaded(v, threshold, newValue, 4);
-    replace_if_custom_multithreaded(v, threshold, newValue, thread::hardware_concurrency());
+    vector<int> v2 = generateRandomVector(n);
+    scalar_mul_without_policy(v, v2);
+//    scalar_mul_with_policy_par(v2, threshold, newValue);
+//    scalar_mul_with_policy_seq(v3, threshold, newValue);
+    scalar_mul_custom(v, v2);
+    scalar_mul_custom_multithreaded(v, v2, 1);
+    scalar_mul_custom_multithreaded(v, v2, 2);
+    scalar_mul_custom_multithreaded(v, v2, 4);
+    scalar_mul_custom_multithreaded(v, v2, thread::hardware_concurrency());
 
     return 0;
 }
